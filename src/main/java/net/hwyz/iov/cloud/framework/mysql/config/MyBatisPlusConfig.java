f7fc7e2399190aa14b1842572ce57dff4eed0e46
hwyz_leo <hwyz.leo.ye@gmail.com>
2024-11-26 21:58:05 +0800

    重构: 调整架构方便更多服务引用

---
+++ src/main/java/net/hwyz/iov/cloud/framework/mysql/config/MyBatisPlusConfig.java
@@ -0,0 +1,24 @@
+package net.hwyz.iov.cloud.framework.mysql.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatisPlus配置
+ *
+ * @author hwyz_leo
+ */
+@Configuration
+public class MyBatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
